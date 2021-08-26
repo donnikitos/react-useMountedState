182e2dc0d78ce052efbd229a592072bd60038eb0
Nikita Nitichevski <me@donnikitos.com>
2021-08-26 22:30:27 +0300

    removed build files from repo

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-declare function useMountedStateHook(): <T extends unknown>($initalValue: T) => (T | (($newVal: T) => void))[];
-export default useMountedStateHook;
-export declare const useMountedState: typeof useMountedStateHook;
